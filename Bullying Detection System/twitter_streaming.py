177b91d0dfae0d20acd1fc9704fc5d2f1fd3de2d
cloud-vr <gvsreyes@gmail.com>
2017-08-03 13:11:29 +0800

    Added initial python script that extract public tweets made in the Philippines.

---
+++ Bullying Detection System/twitter_streaming.py
@@ -0,0 +1,47 @@
+"""
+The code below were heavily based on the tutorial found in http://socialmedia-class.org/twittertutorial.html
+
+The code was modified to extract tweets the were made in the Philippines.
+"""
+
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the necessary methods from "twitter" library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+# Initiate the connection to Twitter Streaming API
+twitter_stream = TwitterStream(auth=oauth)
+
+# filtered by Philippine location
+# coordinates were determined using http://boundingbox.klokantech.com/
+iterator = twitter_stream.statuses.filter(locations="116.982421875,5.4847680181,127.001953125,18.7295019991")
+
+# Print each tweet in the stream to the screen
+# Here we set it to stop after getting 1000 tweets.
+# You don't have to set it to stop, but can continue running
+# the Twitter API to collect data for days or even longer.
+tweet_count = 1000
+for tweet in iterator:
+    tweet_count -= 1
+    # Twitter Python Tool wraps the data returned by Twitter
+    # as a TwitterDictResponse object.
+    # We convert it back to the JSON format to print/score
+    print json.dumps(tweet)
+
+    # The command below will do pretty printing for JSON data, try it out
+    # print json.dumps(tweet, indent=4)
+
+    if tweet_count <= 0:
+        break
